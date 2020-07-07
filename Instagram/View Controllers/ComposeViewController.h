0ae5403d35fd131888ae6c58bbdcd1293eeca022
Ria Vora <riavora@fb.com>
2020-07-06 20:59:58 -0700

    added photos and added a compose view controller and navigator

---
+++ Instagram/View Controllers/ComposeViewController.h
@@ -0,0 +1,17 @@
+//
+//  ComposeViewController.h
+//  Instagram
+//
+//  Created by Ria Vora on 7/6/20.
+//  Copyright © 2020 Ria Vora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ComposeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
