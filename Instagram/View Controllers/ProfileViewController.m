f93f6fadda03afea5aa97b764da1d642b6d18625
Ria Vora <riavora11@gmail.com>
2020-07-08 15:34:26 -0700

    clicking on a profile post collection cell takes you to the detail view!

--- Instagram/View Controllers/ProfileViewController.m
+++ Instagram/View Controllers/ProfileViewController.m
@@ -9,6 +9,7 @@
 #import "ProfileViewController.h"
 #import <Parse/Parse.h>
 #import "PostCollectionCell.h"
+#import "DetailsViewController.h"
 
 @interface ProfileViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
 
@@ -81,14 +82,21 @@
 
 
 
-/*
+
+
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    
+    UICollectionViewCell *tappedCell = sender;
+    NSIndexPath *indexPath = [self.collectionView indexPathForCell:tappedCell];
+    Post *post = self.posts[indexPath.row];
+    
+    DetailsViewController *detailsViewController = [segue destinationViewController];
+    
+    detailsViewController.post = post;
+
 }
-*/
+
 
 @end
